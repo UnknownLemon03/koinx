58ebdb81783eec64fdacb2f2067a287c540e72df
UnknownLemon03 <lemon.sunomono03@gmail.com>
2025-06-06 10:20:22 +0530

    added indexing for crypto

--- api-server/src/models/Crypto.ts
+++ api-server/src/models/Crypto.ts
@@ -4,10 +4,10 @@ const cryptoSchema = new mongoose.Schema({
     usd:{ type: Number, required: true },
     usd_market_cap:{ type: Number, required: true },
     usd_24h_change:{ type: Number, required: true },
-    name: { type: String, required: true }
+    name: { type: String, required: true}
 });
-
-
+// indexing for faster queries
+cryptoSchema.index({ name: 1 });
 
 const CryptoDB = mongoose.model("Crypto", cryptoSchema);
 export default CryptoDB;
